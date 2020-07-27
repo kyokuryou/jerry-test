578bf2791bb81c5c1698617551bd86add86edf4d
CNYKGW\85890327 <kyokuryou@qq.com>
2020-07-27 16:48:11 +0800

    上传字体

--- src/main/java/com/jerry/test/controller/TestController.java
+++ src/main/java/com/jerry/test/controller/TestController.java
@@ -3,8 +3,10 @@ package com.jerry.test.controller;
 import com.jerry.context.common.InputModel;
 import com.jerry.context.common.PlatformType;
 import com.jerry.shiro.servlet.ShiroDelegatingServiceServlet;
+import javax.servlet.ServletException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -17,5 +19,10 @@ public class TestController extends ShiroDelegatingServiceServlet {
   @Override
   protected MethodMetadata methodMetadata(String target, String method, InputModel model) {
     return super.methodMetadata(target + "Service", method, model);
+  }
+
+  @Override
+  public ModelAndView login() throws ServletException {
+    return modelResolver.getIndexView("main");
   }
 }
